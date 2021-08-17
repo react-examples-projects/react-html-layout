6db6813907bd4e142419e1f9856b8add4585a5e9
Libardo <libardojesusrengifo129@gmail.com>
2021-08-17 13:56:16 -0400

    dev: nav-menu footer

--- js/components/navbar/Navbar.js
+++ js/components/navbar/Navbar.js
@@ -4,13 +4,25 @@ export default function Navbar() {
   const [active, setActive] = React.useState(false);
   return (
     <nav role="navigation" aria-label="Menu de navegación" className="nav">
-      <header className="nav-header"></header>
+      <header className="nav-header">
+        <h3>Menu prueba</h3>
+      </header>
       <div className="nav-menu">
         <NavbarItem title="Home" icon="home" />
         <NavbarItem title="About" icon="help-circle" />
         <NavbarItem title="Account" icon="user" />
         <NavbarItem title="Settings" icon="settings" />
         <NavbarItem title="Logout" icon="log-out" />
+
+        <footer className="nav-menu-footer">
+          <img src="../../../img/menu_footer.svg" className="fluid full" />
+
+          <h4 className="nav-menu-footer-title">Enlaces</h4>
+          <p className="nav-menu-footer-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
+            eligendi!
+          </p>
+        </footer>
       </div>
     </nav>
   );
